425ffca2a62e129baeaaf4826dcedbd078e2dd4a
Matěj Pešl <matejpesl1@gmail.com>
2023-02-26 14:04:43 +0100

    customize init

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["cs"],
+    defaultLocale: "cs",
   },
 };
 export default config;
