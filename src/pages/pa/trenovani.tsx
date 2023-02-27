97dd9d7201b72f9e40a0bececefbb54e7c6e5236
Matěj Pešl <matejpesl1@gmail.com>
2023-02-27 21:23:50 +0100

    split only the first occurence of splitter

--- src/pages/pa/trenovani.tsx
+++ src/pages/pa/trenovani.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 import DataFormatter, { Data } from "~/utils/DataFormatter";
 import { useRouter } from "next/navigation";
 import { Button, Card } from "@mantine/core";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion";
+
 
 const PaTraining: NextPage = () => {
   const [data, setData] = useState<Data[] | null>(null);
@@ -16,19 +16,32 @@ const PaTraining: NextPage = () => {
   const [index, setIndex] = useState<number>(0);
   const router = useRouter();
 
+  const [isInitialLoading, setIsInitialLoading] = useState<boolean>(true);
+
   api.methods.get.useQuery({method: Methods.PA}, {
     onSuccess: (data) => {
       if (!data) return router.push(paths.paEdit);
 
       const parsed = DataFormatter.parse(data);
-      // Shuffle it randomly.
-      for (let i = parsed.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [parsed[i], parsed[j]] = [parsed[j]!, parsed[i]!];
-      }
+      shuffle(parsed);
       setData(parsed);
-    }
+    },
+
+    onSettled: () => setIsInitialLoading(false)
   });
+
+  function shuffle(data: Data[]): void {
+    for (let i = data.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [data[i], data[j]] = [data[j]!, data[i]!];
+    }
+  }
+
+  function restart() {
+    shuffle(data!);
+    setIndex(0);
+    setShowValue(false);
+  }
 
   function advance() {
     setIndex(index + 1);
@@ -36,32 +49,56 @@ const PaTraining: NextPage = () => {
   }
 
     return (
-        <div className={""}>
+        <div className={"overflow-hidden"}>
           <BackButton href={paths.pa}/>
             <h1 className="text-4xl text-center font-extrabold tracking-tight text-white sm:text-[5rem]">
                 Trénování
             </h1>
-          <Card className={"relative aspect-square flex rounded-lg flex-col gap-4 mt-28 "}>
-            <h2 className={"text-4xl mt-20 text-center font-bold tracking-tight text-white sm:text-[3rem]"}>
-              {!data?.[0] && "Žádná data"}
-                {/*{isInitialLoading && "Načítám data..."}*/}
-              {data?.[index] && (showValue ? data[index]!.value : data[index]!.key)}
-              {data && index >= data.length && "Konec"}
-              {/*{data && index >= data.length && "Konec"}*/}
-            </h2>
+          <AnimatePresence>
+            <motion.div
+              initial={{opacity: 0, x: 100}}
+              animate={{opacity: 1, x: 0}}
+              exit={{opacity: 0, x: -100}}
+              transition={{duration: 0.3}}
+            >
+              <Card key={data?.[index] ? data[index]!.key : undefined} className={"relative aspect-square flex rounded-xl flex-col gap-4 mt-28 "}>
+                <h2 className={"text-4xl mt-20 text-center font-bold tracking-tight text-white sm:text-[3rem]"}>
+                  {!data?.[0] && (isInitialLoading ? "Načítání..." : "Žádná data")}
+                  {data?.[index] && (showValue ? data[index]!.value : data[index]!.key)}
+                  {data && index >= data.length && "Konec"}
+                </h2>
 
-            <div className={"absolute bottom-0 left-0 right-0 flex flex-row"}>
-              <Button variant={"default"} className={"h-14 bg-blue-200/5 rounded-none w-full"}
-                      onClick={() => setShowValue(true)}>
-                Ukázat
-              </Button>
-              <Button variant={"default"} className={"h-14 bg-blue-200/20 rounded-none w-full"}
-                      onClick={advance}>
-                Další
-              </Button>
-            </div>
-          </Card>
+                <div className={"absolute bottom-0 left-0 right-0 flex flex-row"}>
+                  {
+                    data && index < data.length &&
+                    <GameButtons showButtOnClick={() => setShowValue(true)} nextButtOnClick={() => advance()}/>
+                  }
+                  {
+                    data && index >= data.length &&
+                    (<Button variant={"default"} className={"w-full h-14 bg-blue-200/20 rounded-none w-full"} onClick={restart}>
+                      Restartovat
+                    </Button>)
+                  }
+                </div>
+              </Card>
+            </motion.div>
+          </AnimatePresence>
         </div>
     );
+}
+
+const GameButtons = (props: {showButtOnClick: () => void, nextButtOnClick: () => void},) => {
+  return (
+    <>
+      <Button variant={"default"} className={"h-14 bg-blue-200/5 rounded-none w-full"}
+              onClick={props.showButtOnClick}>
+        Ukázat
+      </Button>
+      <Button variant={"default"} className={"h-14 bg-blue-200/20 rounded-none w-full"}
+              onClick={props.nextButtOnClick}>
+        Další
+      </Button>
+    </>
+  )
 }
 export default PaTraining;
--- src/utils/DataFormatter.ts
+++ src/utils/DataFormatter.ts
@@ -1,49 +1,39 @@
 export type Data = {key: string; value: string};
 export default class DataFormatter {
   public static readonly splitter = ": ";
-  
-  static toString = (map: Map<string, string>): string => {
-    let result = "";
-    map.forEach((value, key) => result += `${key}${DataFormatter.splitter}${value}\n`);
-    return result;
-  }
 
-  static isDataValid = (str: string): boolean => {
-    const values = str.split("\n");
-    for (let i = 0; i < values.length; i++) {
-      const splitted = values[i]!.split(this.splitter);
-      if (splitted.length != 2 || !splitted[0] || !splitted[1]) return false;
+  static isDataValid = (data: string): boolean => {
+    const lines = data.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      const [key, value] = lines[i]!.substring(lines[i]!.indexOf(this.splitter));;
+      if (!key || !value) return false;
     }
     return true;
   }
 
-  static parse = (str: string): Data[] => {
-    const arr: Data[] = [];
+  static parse = (data: string): Data[] => {
+    const parsed: Data[] = [];
 
-    const values = str.split("\n");
-    values.forEach((value, index) => {
-      const [key, val] = value.split(this.splitter);
-      arr.push({key: key!, value: val!});
+    const lines = data.split("\n");
+    console.log("-> lines", lines);
+    lines.forEach((line, index) => {
+      const [key, ...rest] = line.split(this.splitter);
+      const val = rest.join(this.splitter);
+
+      console.log("-> key", key);
+      console.log("-> val", val);
+
+      parsed.push({key: key!, value: val!});
     });
 
-    return arr;
+    return parsed;
   }
 
-  static convertSplitter(str: string, splitter: string): string {
-    if (splitter === this.splitter) return str;
+  static convertSplitter(data: string, splitter: string): string {
+    if (splitter === this.splitter) return data;
     // replace the first occurence of "splitter" on each line with ": ".
-    const lines = str.split("\n");
+    const lines = data.split("\n");
     const result = lines.map(line => line.replace(splitter, this.splitter));
     return result.join("\n");
-  }
-
-  static toMap = (str: string): Map<string, string> => {
-    const map = new Map<string, string>();
-    const values = str.split("\n");
-    values.forEach(value => {
-      const [key, val] = value.split(this.splitter);
-      map.set(key!, val!);
-    });
-    return map;
   }
 }
